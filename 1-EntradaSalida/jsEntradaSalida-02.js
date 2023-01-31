154b6bb077cf31b5c45fb59eed73dea9d7eb4ab3
grxywarxn <lclaros800@gmail.com>
2023-01-31 15:19:32 -0300

    ejercicio 3 resuelto

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,3 +1,4 @@
+// Luis Claros 
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,12 @@
+// Luis Claros 
 /*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+	alert("Su nomre es " + nombreIngresado);
 
 }
 
