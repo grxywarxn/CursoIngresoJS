0895e86b6c8dbad01a01ba5aca4acf0d6d7243cb
unknown <lclaros800@gmail.com>
2023-01-31 13:05:45 -0300

    ejercicio 2 resuelto

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
+// Luis Claros
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla");
 }
 
