85d7d1e50608f1aa6272d6c9aedd0cbbb732a279
grxywarxn <lclaros800@gmail.com>
2023-02-01 21:32:52 -0300

    ejercicio 7 resuelto

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,3 +1,4 @@
+// Luis Claros
 /*
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
@@ -5,21 +6,72 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let primerNum;
+	let segundoNum;
+	let resultadoSuma;
+	let mensajeSuma;
+
+	primerNum = document.getElementById("txtIdNumeroUno").value;
+	segundoNum = document.getElementById("txtIdNumeroDos").value;
+
+	primerNum = parseInt(primerNum);
+	segundoNum = parseInt(segundoNum);
+
+	resultadoSuma = primerNum + segundoNum;
+	mensajeSuma = "La suma de los dos numeros es igual a " + resultadoSuma;
+	alert(mensajeSuma);
 }
 
 function restar()
 {
-	
+	let primerNum;
+	let segundoNum;
+	let resultadoResta;
+	let mensajeResta;
+
+	primerNum = document.getElementById("txtIdNumeroUno").value;
+	segundoNum = document.getElementById("txtIdNumeroDos").value;
+
+	primerNum = parseInt(primerNum);
+	segundoNum = parseInt(segundoNum);
+
+	resultadoResta = primerNum - segundoNum;
+	mensajeResta = "La resta de los dos numeros es igual a " + resultadoResta;
+	alert(mensajeResta);	
 }
 
 function multiplicar()
 { 
-	
+	let primerNum;
+	let segundoNum;
+	let resultadoMultiplicacion;
+	let mensajeMultiplicacion;
+
+	primerNum = document.getElementById("txtIdNumeroUno").value;
+	segundoNum = document.getElementById("txtIdNumeroDos").value;
+
+	primerNum = parseInt(primerNum);
+	segundoNum = parseInt(segundoNum);
+
+	resultadoMultiplicacion = primerNum * segundoNum;
+	mensajeMultiplicacion = "La multiplicación de los dos numeros es igual a " + resultadoMultiplicacion;
+	alert(mensajeMultiplicacion);	
 }
 
 function dividir()
 {
-	
-}
+	let primerNum;
+	let segundoNum;
+	let resultadoDivision;
+	let mensajeDivision;
 
+	primerNum = document.getElementById("txtIdNumeroUno").value;
+	segundoNum = document.getElementById("txtIdNumeroDos").value;
+
+	primerNum = parseInt(primerNum);
+	segundoNum = parseInt(segundoNum);
+
+	resultadoDivision = primerNum / segundoNum;
+	mensajeDivision = "La división de los dos numeros es igual a " + resultadoDivision;
+	alert(mensajeDivision);	
+}
