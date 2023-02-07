72e7320f197766ada1504aeb41591bf3873ad595
grxywarxn <lclaros800@gmail.com>
2023-02-07 09:41:34 -0300

    if ej 2 resuelto

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,17 @@
+//Luis Claros
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad
+	let mensaje
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if (edad >= 18){
+
+		mensaje = "Sos mayor de edad"
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
