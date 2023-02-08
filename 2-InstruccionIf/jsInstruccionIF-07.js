3dbe43329762a15d207a6e127133fb3d596e90d5
grxywarxn <lclaros800@gmail.com>
2023-02-08 09:32:19 -0300

    if ej 7 resuelto

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,18 @@
+//Luis Claros
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad;
+	let estadoCivil;
+	let mensaje;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if(edad < 18 && estadoCivil != "Soltero")
+	{
+		mensaje = "Es muy pequeño para NO ser Soltero.";
+		alert(mensaje);
+	}
 
 }//FIN DE LA FUNCIÓN
