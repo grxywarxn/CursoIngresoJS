5c252427c3d4359859201b91d452b73fa3848ec1
grxywarxn <lclaros800@gmail.com>
2023-02-09 21:41:47 -0300

    switch ej 4 resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -12,16 +12,16 @@ function mostrar()
 	
 	switch(mesDelAnio){
 		case "Febrero" :
-			mensaje = "tiene 28 dias"
+			mensaje = "tiene 28 dias";
 			break;
 		case "Abril" :
 		case "Junio" :
 		case "Septiembre" :
 		case "Noviembre" :
-			mensaje = "tiene 30 dias"
+			mensaje = "tiene 30 dias";
 			break;
 		default : 
-			mensaje = "tiene 31 dias"
+			mensaje = "tiene 31 dias";
 			break;
 	}
 	alert(mensaje);
