74d4d619059a38a0687b15202b790e7003fc037d
grxywarxn <lclaros800@gmail.com>
2023-02-09 21:58:58 -0300

    switch ej 7 resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,25 @@
+//Luis Claros
+/*Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste */
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado;
+	let mensaje;
 
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+
+	switch(destinoIngresado){
+		case "Bariloche" :
+			mensaje = "Bariloche se encuentra al Oeste del país.";
+			break;
+		case "Cataratas" :
+			mensaje = "Cataratas se encuentra al Norte del país.";
+			break;
+		case "Mar del plata" :
+			mensaje = "Mar del plata se encuentra al Este del país.";
+			break;
+		case "Ushuaia" :
+			mensaje = "Ushuaia se encuentra al Sur del país.";
+			break;
+	}
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
