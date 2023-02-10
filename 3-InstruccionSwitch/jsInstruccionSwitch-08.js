68394bbbb6463019f0e4484cdf78418f5201fbe4
grxywarxn <lclaros800@gmail.com>
2023-02-09 22:04:19 -0300

    switch ej 8 resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,25 @@
+//Luis Claros
+/*Al seleccionar un destino informar si hace FRIO o CALOR en ese destino */
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado;
+	let mensaje;
 
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+
+	switch(destinoIngresado){
+		case "Bariloche" :
+			mensaje = "En Bariloche hace FRIO.";
+			break;
+		case "Cataratas" :
+			mensaje = "En Cataratas hace CALOR.";
+			break;
+		case "Mar del plata" :
+			mensaje = "En Mar del plata hace CALOR.";
+			break;
+		case "Ushuaia" :
+			mensaje = "En Ushuaia hace FRIO.";
+			break;
+	}
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
