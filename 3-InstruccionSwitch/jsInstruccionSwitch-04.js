948247d0064629d84e94b3cbc3f735f358a8422e
grxywarxn <lclaros800@gmail.com>
2023-02-09 21:35:51 -0300

    switch ej 4 resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,29 @@
+//Luis Claros
+/*al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesDelAnio;
+	let mensaje;
 
-
+	mesDelAnio = document.getElementById("txtIdMes").value;
+	
+	switch(mesDelAnio){
+		case "Febrero" :
+			mensaje = "tiene 28 dias"
+			break;
+		case "Abril" :
+		case "Junio" :
+		case "Septiembre" :
+		case "Noviembre" :
+			mensaje = "tiene 30 dias"
+			break;
+		default : 
+			mensaje = "tiene 31 dias"
+			break;
+	}
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
