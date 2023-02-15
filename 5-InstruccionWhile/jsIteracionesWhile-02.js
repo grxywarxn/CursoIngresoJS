74222c2a29c75d12e78414b1aadae18597fa43a3
grxywarxn <lclaros800@gmail.com>
2023-02-14 21:50:57 -0300

    while ej 2 resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,10 +1,21 @@
+//Luis Claros
 /*
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
+	//DECLARO VARIABLES
+	let numero;
+	let mensaje;
 
+	numero = 11
+	//PROCESO
+	while(numero > 1)
+	{
+		numero = numero - 1;
+		console.log(numero);
+		mensaje = "fuera del bucle";
+	}
+	//INFORMO
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
