372a9e81702dee01de478858cd4cd5e0cf87c811
grxywarxn <lclaros800@gmail.com>
2023-02-14 21:33:10 -0300

    switch ej 5 correccion

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -6,18 +6,17 @@ function mostrar()
 	let horaDelDia;
 	let mensaje;
 
-	horaDelDia = document.getElementById("txtIdHora").value;
+	horaDelDia = parseInt(document.getElementById("txtIdHora").value);
 
 	switch(horaDelDia){
-		case "7":
-		case "8":
-		case "9":
-		case "10":
-		case "11" :
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
 			mensaje = "Es de mañana.";
 			break;
-		default :
-			break;
 	}
+	
 	alert(mensaje);
 }//FIN DE LA FUNCIÓN
