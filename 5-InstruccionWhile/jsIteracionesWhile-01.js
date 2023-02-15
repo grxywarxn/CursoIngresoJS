8fa84924ffc338e4e9a2e16fa40407552f347b90
grxywarxn <lclaros800@gmail.com>
2023-02-14 21:34:35 -0300

    -

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,12 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	let numero;
+
+	numero = 1
+
+	while(numero < 11)
+	{
+		
+	}
 }//FIN DE LA FUNCIÓN
