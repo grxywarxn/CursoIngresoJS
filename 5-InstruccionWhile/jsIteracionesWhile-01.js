14f4c6dadba5343e817f762dd6f46c944943d22e
grxywarxn <lclaros800@gmail.com>
2023-02-14 21:50:49 -0300

    while ej 1 resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,14 +1,21 @@
+//Luis Claros
 /*
 al presionar el botón mostrar 10 repeticiones 
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
+	//DECLARO VARIABLES
 	let numero;
+	let mensaje;
 
-	numero = 1
-
-	while(numero < 11)
+	numero = 0
+	//PROCESO
+	while(numero < 10)
 	{
-		
+		numero = numero + 1;
+		console.log(numero);
+		mensaje = "fuera del bucle";
 	}
+	//INFORMO
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
