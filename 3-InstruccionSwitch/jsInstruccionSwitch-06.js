e74a8b1f3bc1758dee58d67e631172a632bfbc95
grxywarxn <lclaros800@gmail.com>
2023-02-14 21:33:22 -0300

    switch ej 6 correccion

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -8,37 +8,37 @@ function mostrar() {
 	let horaDelDia;
 	let mensaje;
 
-	horaDelDia = document.getElementById("txtIdHora").value;
+	horaDelDia = parseInt(document.getElementById("txtIdHora").value);
 
 	switch (horaDelDia) {
-		case "7":
-		case "8":
-		case "9":
-		case "10":
-		case "11":
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
 			mensaje = "Es de mañana.";
 			break;
-		case "12":
-		case "13":
-		case "14":
-		case "15":
-		case "16":
-		case "17":
-		case "18":
-		case "19":
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
 			mensaje = "Es de tarde.";
 			break;
-		case "20":
-		case "21":
-		case "22":
-		case "23":
-		case "24":
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		case "5":
-		case "6":
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
 			mensaje = "Es de noche.";
 			break;
 		default : 
