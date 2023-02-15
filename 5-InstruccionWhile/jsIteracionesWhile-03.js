0d6d930f4feb187e570b1627787d85eedaa46dec
grxywarxn <lclaros800@gmail.com>
2023-02-14 22:17:24 -0300

    while ej 3 resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,19 @@
+//Luis Claros
 /*
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	let claveIngresada;
+	let mensaje;
+
+	claveIngresada = prompt("Ingrese la clave");
+
+	while(claveIngresada != "utn750")
+	{
+		claveIngresada = prompt("Ingrese una clave valida");
+	}
+
+	mensaje = "Clave correcta";
+	alert(mensaje);
 	
 }//FIN DE LA FUNCIÓN
