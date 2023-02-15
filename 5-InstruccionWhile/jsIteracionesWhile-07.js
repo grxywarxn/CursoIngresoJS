9a2a6b540da38b9edfb9003d877ff3c6fb04be50
grxywarxn <lclaros800@gmail.com>
2023-02-15 11:13:53 -0300

    while ej 7 resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,18 +1,39 @@
+//Luis Claros 
 /*
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+	let contador;
+	let acumulador;
+	let numeroIngresado;
+	let promedio;
+	let respuesta;
+	
+	numeroIngresado = prompt("Ingrese un número");
+	numeroIngresado = parseInt(numeroIngresado);
+	contador = 0;
+	acumulador = 0;
+	respuesta = "si";
 
+	while(respuesta == "si" || respuesta == "SI" || respuesta == "Si" || respuesta == "sI")
+	{
+		numeroIngresado = prompt("Ingrese un número");
+		numeroIngresado = parseInt(numeroIngresado);
+		
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado = parseInt(prompt("Error, ingrese un número válido"));
+			numeroIngresado = parseInt(numeroIngresado);
+		}
+		contador = contador + 1;
+		acumulador = acumulador + numeroIngresado;
+		respuesta = prompt("¿Desea seguir ingresando números? (si/no)")
+	}	
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+	promedio = acumulador / contador;
 
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
 }//FIN DE LA FUNCIÓN
