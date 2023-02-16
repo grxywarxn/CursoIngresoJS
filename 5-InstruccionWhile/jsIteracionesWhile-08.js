0918b29fc0746052d87fbee2da4d4cbba506734b
grxywarxn <lclaros800@gmail.com>
2023-02-15 21:25:29 -0300

    while ej 8 resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,39 @@
+//Luis Claros
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+	let numeroIngresado;
+	let respuesta;
+	let sumaPositivos;
+	let multiplicacionNegativos;
 
+	
+	sumaPositivos = 0;
+	multiplicacionNegativos = 1;
+	respuesta = "si";
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+	while(respuesta == "si" || respuesta == "SI" || respuesta == "Si" || respuesta == "sI")
+	{
+		numeroIngresado = parseInt(prompt("Ingrese un numero:"));
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado = parseInt(prompt("Error, ingrese un número válido"));
+		}
+		if(numeroIngresado > 0)
+		{
+			sumaPositivos = sumaPositivos + numeroIngresado;
+		}
+		else
+		{
+			multiplicacionNegativos = multiplicacionNegativos * numeroIngresado;
+		}
+	
+		respuesta = prompt("¿Quiere ingresar otro número?");
+	}
+
+	document.getElementById("txtIdSuma").value = sumaPositivos;
+	document.getElementById("txtIdProducto").value = multiplicacionNegativos;
 
 }//FIN DE LA FUNCIÓN
