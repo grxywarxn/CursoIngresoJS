ff677290a390597a118e9ec4d302d663a0892ef6
grxywarxn <lclaros800@gmail.com>
2023-02-16 11:03:19 -0300

    -

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -12,18 +12,41 @@ hasta que el usuario quiera, mostrar:
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	
+	let respuesta;
+	let numeroIngresado;
+	let sumaNegativos; //1
+	let sumaPositivos; //2
+	let contadorPositivos; // 3
+	let contadorNegativos; // 4
+	let contadorCeros; // 5
+	let contadorPares; //6
+	let promedioPositivos; // 7 
+	let promedioNegativos; // 8
+	let diferenciaPositivosNegativos; // 9
 
 	respuesta="si";
+	sumaNegativos = 0;
+	sumaPositivos = 0;
+	contadorCeros = 0;
+	contadorNegativos = 0;
+	contadorPositivos = 0;
 
 	while(respuesta=="si")
 	{
-		
+		numeroIngresado = parseInt(prompt("Ingrese Un número"));
+		while(isNaN(numeroIngresado))
+		{
+			parseInt(prompt("Error, ingrese un número válido"));
+		}
+
+		if(numeroIngresado < 0)
+		{
+			contadorNegativos = contadorNegativos + 1;
+			sumaNegativos = sumaNegativos + numeroIngresado;
+		}
 		respuesta=prompt("desea continuar?");
-	}//fin del while
+	}
 
 	document.write("la suma de negativos es :"+sumaNegativos);
-}//FIN DE LA FUNCIÓN
+}
