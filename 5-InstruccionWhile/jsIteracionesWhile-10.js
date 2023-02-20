411c68296da6e77e40d445cfe3deb6991697cea9
grxywarxn <lclaros800@gmail.com>
2023-02-20 17:31:14 -0300

    while ej 10 resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -1,3 +1,4 @@
+//Luis Claros
 /*
 Al presionar el botón pedir  números  
 hasta que el usuario quiera, mostrar:
@@ -21,6 +22,8 @@ function mostrar()
 	let contadorNegativos; // 4
 	let contadorCeros; // 5
 	let contadorPares; //6
+	let parPositivo;
+	let parNegativo;
 	let promedioPositivos; // 7 
 	let promedioNegativos; // 8
 	let diferenciaPositivosNegativos; // 9
@@ -31,22 +34,55 @@ function mostrar()
 	contadorCeros = 0;
 	contadorNegativos = 0;
 	contadorPositivos = 0;
-
-	while(respuesta=="si")
+	contadorPares = 0;
+	promedioPositivos = 0;
+	promedioNegativos = 0;
+	
+	while(respuesta == "si")
 	{
-		numeroIngresado = parseInt(prompt("Ingrese Un número"));
+		numeroIngresado = parseInt(prompt("Ingrese un número:"));
 		while(isNaN(numeroIngresado))
 		{
-			parseInt(prompt("Error, ingrese un número válido"));
+			numeroIngresado = parseInt(prompt("Error, ingrese un número válido"));
 		}
-
 		if(numeroIngresado < 0)
 		{
 			contadorNegativos = contadorNegativos + 1;
 			sumaNegativos = sumaNegativos + numeroIngresado;
 		}
-		respuesta=prompt("desea continuar?");
-	}
+		else
+		{
+			if(numeroIngresado > 0)
+			{
+				contadorPositivos = contadorPositivos + 1;
+				sumaPositivos = sumaPositivos + numeroIngresado;
+			}
+			else
+			{
+				contadorCeros = contadorCeros + 1;
+			}
+		}
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+		parPositivo = numeroIngresado % 2;
+		parNegativo = numeroIngresado % -2;
+
+		if(parPositivo == 0 || parNegativo == 0)
+		{
+			contadorPares = contadorPares + 1;
+		}
+		respuesta = prompt("¿Desea seguír ingresando números?");
+	}
+	promedioPositivos = sumaPositivos / contadorPositivos;
+	promedioNegativos = sumaNegativos / contadorNegativos;
+	diferenciaPositivosNegativos = promedioPositivos - promedioNegativos;
+
+	console.log("la suma de negativos es : " + sumaNegativos);
+	console.log("la suma de positivos es : " + sumaPositivos);
+	console.log("la cantidad de positivos es : " + contadorPositivos);
+	console.log("la cantidad de negativos es : " + contadorNegativos);
+	console.log("la cantidad de ceros es : " + contadorCeros);
+	console.log("la cantidad de pares es : " + contadorPares);
+	console.log("el promedio de positivos es : " + promedioPositivos);
+	console.log("el promedio de negativos es : " + promedioNegativos);
+	console.log("la diferencia entre positivos y negativos es : " + diferenciaPositivosNegativos);
 }
