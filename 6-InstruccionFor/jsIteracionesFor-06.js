ec3a19eb15d4975c39a7ce0ff78ffd4a53cabd92
grxywarxn <lclaros800@gmail.com>
2023-02-21 12:05:47 -0300

    for ej 6 resuelto

--- 6-InstruccionFor/jsIteracionesFor-06.js
+++ 6-InstruccionFor/jsIteracionesFor-06.js
@@ -1,7 +1,25 @@
+//Luis Claros
 function mostrar()
 {
-	alert("ok");
+	let numeroIngresado;
+	let contadorPares;
+	let mensaje;
+	let i;
 
+	contadorPares = 0;
+ 
+	numeroIngresado = parseInt(prompt("Ingrese un numero."));
+
+	for(i = 1; i <= numeroIngresado; i++)
+	{
+		if(i % 2 == 0)
+		{
+			contadorPares = contadorPares + 1;
+			console.log(i)
+		}
+	}
+	mensaje = "La cantidad de pares encontrados es: " + contadorPares;
+	console.log(mensaje);
 
 
 }//FIN DE LA FUNCIÓN
