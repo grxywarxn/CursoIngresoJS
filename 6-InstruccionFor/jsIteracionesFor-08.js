4531553cda6ed8edc8715ba6757b60667202c8c8
grxywarxn <lclaros800@gmail.com>
2023-02-21 12:06:14 -0300

    for ej 8 resuelto

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,28 @@
+//Luis Claros
 function mostrar()
 {
+	let numeroIngresado;
+	let contadorDivisores;
+	let mensaje;
+	let i;
+	contadorDivisores = 0;
+ 
+	numeroIngresado = parseInt(prompt("Ingrese un numero."));
 
-
-	alert("ok");
-
+	for(i = 1; i <= numeroIngresado; i++)
+	{
+		if(numeroIngresado % i == 0)
+		{
+			contadorDivisores = contadorDivisores + 1;
+		}
+	}
+	if(contadorDivisores == 2)
+	{
+		mensaje = numeroIngresado + " Es primo";
+	}
+	else
+	{
+		mensaje = numeroIngresado + " No es primo"
+	}
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
