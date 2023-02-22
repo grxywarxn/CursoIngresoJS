917e3ff726145371edaa55fa541cf24fa0a6c80c
grxywarxn <lclaros800@gmail.com>
2023-02-21 21:34:31 -0300

    while 9 bis incompleto

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,7 +1,8 @@
 //Luis Claros
 /*
-Al presionar el botón pedir  números  hasta que el usuario quiera,
-mostrar el número máximo y el número mínimo ingresado.*/
+While 9 bis: Al presionar el botón pedir  números  hasta que el usuario quiera,
+mostrar el número máximo y el número mínimo ingresado.
+El menor de los pares y el mayor de los negativos ...(solo si hay)*/
 function mostrar()
 {
 	let numeroIngresado;
@@ -9,6 +10,9 @@ function mostrar()
 	let numeroMinimo;
 	let respuesta;
 	let bandera;
+	let numeroPar;
+	let menorPar;
+	let mayorNegativo;
 
 	respuesta = "si";
 	bandera = 0;
@@ -22,8 +26,9 @@ function mostrar()
 		}
 		if(bandera == 0)
 		{
-			numeroMaximo=numeroIngresado;
-			numeroMinimo=numeroIngresado;
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+			menorPar = numeroIngresado;
 			bandera = 1;
 		}
 		if(numeroIngresado > numeroMaximo)
@@ -32,11 +37,21 @@ function mostrar()
 		}
 		else
 		{
+			if(numeroIngresado < numeroMinimo)
+			{
 			numeroMinimo = numeroIngresado;
+			}
 		}
 
+		if(numeroIngresado % 2 == 0 && numeroIngresado < menorPar)
+		{
+			menorPar = numeroIngresado;	
+		}
+	
 		respuesta = prompt("¿desea continuar?");
 	}
 	document.getElementById("txtIdMaximo").value = numeroMaximo;
 	document.getElementById("txtIdMinimo").value = numeroMinimo;
+	console.log("El menore de los pares ingresados es: " + menorPar);
+
 }
