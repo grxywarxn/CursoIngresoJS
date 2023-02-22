14f2813f6f7a14dc209874a136e365c9fb7e0a67
grxywarxn <lclaros800@gmail.com>
2023-02-21 21:34:42 -0300

    for ej 5 resuelto

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,4 +1,10 @@
 //Luis Claros
+/*For 5 bis, Realizar el programa que permita el ingreso de las notas (validar entre 0 y 10) , 
+el sexo (validar el sexo “f” o “m” o "b") de 7 alumnos, informar por console.log: 
+a) El promedio de las notas totales. 
+b) La nota más baja y el sexo de esa persona. 
+c) La cantidad de varones que su nota haya sido mayor o igual a 6.
+d) La cantidad de alumnos desaprobados (nota < 4)*/
 function mostrar()
 {
 	let repetir;
