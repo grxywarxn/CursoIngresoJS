3f9d0bfe9e66a8aa5848279ec84eb4aa2005130c
grxywarxn <lclaros800@gmail.com>
2023-02-22 21:54:01 -0300

    while 9 bis resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,55 +3,69 @@
 While 9 bis: Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.
 El menor de los pares y el mayor de los negativos ...(solo si hay)*/
-function mostrar()
-{
+function mostrar() {
 	let numeroIngresado;
 	let numeroMaximo;
 	let numeroMinimo;
 	let respuesta;
 	let bandera;
-	let numeroPar;
+	let banderaPares;
 	let menorPar;
+	let banderaNegativos;
 	let mayorNegativo;
 
 	respuesta = "si";
-	bandera = 0;
+	bandera = true;
+	banderaPares = true;
+	banderaNegativos = true;
 
-	while(respuesta == "si")
-	{
+	while (respuesta == "si") {
 		numeroIngresado = parseInt(prompt("Ingrese un número:"));
-		while(isNaN(numeroIngresado))
-		{
+		while (isNaN(numeroIngresado)) {
 			numeroIngresado = parseInt(prompt("Error, ingrese un número válido"));
 		}
-		if(bandera == 0)
-		{
+		if (bandera == true) {
 			numeroMaximo = numeroIngresado;
 			numeroMinimo = numeroIngresado;
-			menorPar = numeroIngresado;
-			bandera = 1;
+			bandera = false;
 		}
-		if(numeroIngresado > numeroMaximo)
-		{
-			numeroMaximo = numeroIngresado;
-		}
-		else
-		{
-			if(numeroIngresado < numeroMinimo)
+		if(banderaPares == true){
+			if(numeroIngresado % 2 == 0)
 			{
-			numeroMinimo = numeroIngresado;
+				menorPar = numeroIngresado;
+				banderaPares = false;
+			}
+		}
+		if(banderaNegativos == true){
+			if(numeroIngresado < 0)
+			{
+				mayorNegativo = numeroIngresado;
+				banderaNegativos = false;
+			}
+		}
+		if(numeroIngresado > numeroMaximo) {
+			numeroMaximo = numeroIngresado;
+		}
+		else{
+			if (numeroIngresado < numeroMinimo) {
+				numeroMinimo = numeroIngresado;
 			}
 		}
 
-		if(numeroIngresado % 2 == 0 && numeroIngresado < menorPar)
-		{
-			menorPar = numeroIngresado;	
+		if(banderaPares == false && numeroIngresado < menorPar) {
+			menorPar = numeroIngresado;
 		}
-	
-		respuesta = prompt("¿desea continuar?");
+		if(banderaNegativos == false && numeroIngresado > mayorNegativo){
+			mayorNegativo = numeroIngresado;
+		}
+		respuesta = prompt("¿Desea seguir ingresando números?");
 	}
 	document.getElementById("txtIdMaximo").value = numeroMaximo;
 	document.getElementById("txtIdMinimo").value = numeroMinimo;
-	console.log("El menore de los pares ingresados es: " + menorPar);
-
+	if(banderaPares == false){
+	alert("El menor de los pares ingresados es: " + menorPar);
+	}
+	if(banderaNegativos == false){
+	alert("El mayor de los negativos es: " + mayorNegativo);
+	}
 }
