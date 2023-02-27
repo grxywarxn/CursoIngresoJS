642996119e902737f19a3ae4362462baa0971743
grxywarxn <lclaros800@gmail.com>
2023-02-27 10:48:10 -0300

    sabado ej 1 preparcial

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,97 +1,106 @@
 // Luis Claros
-//ej 1 bis bis
+//* Apellido: Claros
+//* Nombre: Luis
+//* Comisión: B
+//* Tutor: Matias
+/* 
+Ejercicio 1
+La Premier League de Inglaterra necesita un sistema capaz de reunir las estadisticas de los 
+jugadores que la integran.
+Para ellos se pide realizar una app.
+
+Se debe ingresar por cada jugador
+
+-Nombre del jugador
+-Nacionalidad
+-Posición (Arquero, Defensor, Mediocampista o Delantero)
+-Cantidad de Tarjetas Rojas recibidas
+-Goles 
+
+Se necesita saber
+
+a)El goleador del torneo
+b)El jugador Argentino con menos rojas recibidas.
+c)El porcentaje de jugadores Ingleses de la liga
+
+NOTA: Deben poder ingresarse todos números positivos, no se deben pueden ingresar números negativos. */
+
+//ej 1 simulacro parcial
 function mostrar()
 {
     let nombre;
-    let alturaIngresada;
-    let pesoIngresado;
-    let imc;
-    let mensaje;
+    let nacionalidad;
+    let posicion;
+    let tarjetasRojas;
+    let goles;
+    let goleadorTorneo;
+    let cantidadMaximaGoles;
+    let argentinoConMenosRojas;
+    let contadorRojas;
+    let contadorJugadores
+    let contadorJugadoresIngleses;
+    let porcentajeJugadoresIngleses
+    let respuesta;
+    let banderaGoleador;
+    let banderaArgentinoConMenosRojas;
 
-    nombre = prompt("Ingrese su nombre.");
-    pesoIngresado = parseFloat(document.getElementById("txtIdEdad").value);
-    alturaIngresada = parseFloat(prompt("Ingrese su altura."));
-    imc = pesoIngresado / (alturaIngresada * alturaIngresada)
+    respuesta = "si"
+    banderaGoleador = true;
+    banderaArgentinoConMenosRojas = true;
+    contadorJugadores = 0;
+    contadorJugadoresIngleses = 0;
 
-    if(imc <= 18.5){
-        mensaje = nombre + ", usted tiene bajo peso";
-    }else if(imc < 25 ){
-        mensaje = nombre + ", usted tiene peso normal";
-    }else if(imc < 27){
-        mensaje = nombre + ", usted tiene preobesidad";
-    }else if(imc < 30){
-        mensaje = nombre + ", usted tiene obesidad I";
-    }else if(imc < 35){
-        mensaje = nombre + ", usted tiene obesidad II";
-    }else{
-        mensaje = nombre + ", usted tiene obesidad III";
+    while(respuesta == "si"){
+        nombre = prompt("Ingrese su nombre");
+        nacionalidad = prompt("Ingrese su nacionalidad");
+        while(nacionalidad != "argentino" && nacionalidad != "chino" && nacionalidad != "ingles")
+            nacionalidad = prompt("Error. Ingrese una nacionalidad válida: argentino, chino o ingles");
+        posicion = prompt("Ingrese su posición");
+        while(posicion != "arquero" && posicion != "defensor" && posicion != "mediocampista" && posicion != "delantero"){
+            posicion = prompt("Error. Ingrese una posición válida: arquero, defensor, mediocampista o delantero");  
+        }
+        tarjetasRojas = parseInt(prompt("Ingrese la cantidad de tarjetas rojas recibidas"));
+        while(isNaN(tarjetasRojas) || tarjetasRojas < 0){
+            tarjetasRojas = parseInt(prompt("Error. Ingrese la cantidad de tarjetas rojas recibidas"));
+        }  
+        goles = parseInt(prompt("Ingrese la cantidad de goles realizados"));
+        while(isNaN(goles) || goles < 0){
+            goles = parseInt(prompt("Error. Ingrese la cantidad de goles realizados"));
+        }
+
+        if(banderaGoleador == true){
+            goleadorTorneo = nombre;
+            cantidadMaximaGoles = goles;
+            banderaGoleador = false;
+        }else{
+            if(goles > cantidadMaximaGoles){
+                cantidadMaximaGoles = goles;
+                goleadorTorneo = nombre;
+            }
+        }
+
+        if(banderaArgentinoConMenosRojas == true && nacionalidad == "argentino"){
+            argentinoConMenosRojas = nombre;
+            contadorRojas = tarjetasRojas;
+            banderaArgentinoConMenosRojas = false;
+        }else{
+            if(tarjetasRojas < contadorRojas){
+                argentinoConMenosRojas = nombre;
+                contadorRojas = tarjetasRojas;
+            }
+        }
+
+        if(nacionalidad == "ingles"){
+            contadorJugadoresIngleses = contadorJugadoresIngleses + 1;
+            contadorJugadores = contadorJugadores + 1;
+        }else{
+            contadorJugadores = contadorJugadores + 1;
+        }
+        respuesta = prompt("¿Desea seguir ingresando jugadores? si/no");
     }
+    porcentajeJugadoresIngleses = (contadorJugadoresIngleses * 100) / contadorJugadores;
 
-    alert(mensaje);
+    console.log("El goleador del torneo es: " + goleadorTorneo);
+    console.log("El jugador Argentino con menos tarjetas rojas es: " + argentinoConMenosRojas);
+    console.log("El porcentaje de jugadores ingleses de la liga es de " + porcentajeJugadoresIngleses + "%");
 }
-
-
-/*
-ej 1 bis
-{
-    let distanciaViajada;
-    let tiempoDeViaje;
-    let mensaje;
-    let velocidad;
-
-    distanciaViajada = parseInt(prompt("Ingrese la distancia recorrida."));
-    tiempoDeViaje = parseInt(document.getElementById("txtIdEdad").value);
-    velocidad = distanciaViajada / tiempoDeViaje;
-
-    if(velocidad <= 60){
-        mensaje = "muy lento";
-    }else if(velocidad <= 80){
-        mensaje = "lento";
-    }else if(velocidad <= 100){
-        mensaje = "buen ritmo";
-    }else if(velocidad <= 120){
-        mensaje = "ahí de la ley"
-    }else{
-        mensaje = "eso no se hace!"
-    }
-    alert(mensaje);
-    
-
-}
-/*{
-	let edadIngresada;
-	let resultado
-
-	edadIngresada = parseInt(document.getElementById("txtIdEdad").value);
-
-	if (edadIngresada == 15){
-
-		resultado = "niña bonita";
-
-	}else
-	{ 
-		resultado = "tu edad es " + edadIngresada;
-
-	}
- 
-	alert(resultado);
-
-
-}
-/*
-    if(condicion)//verdadero o falso (0) true -> verdadero false -> falso
-    {
-        codigo...;
-    }else
-    {
-        codigo...;
-    }
-
-    operadores aritmeticos:
-    + - * / %
-    operadores relacionales o condicionales
-    < > <= >= == !=
-    operadores logicos
-    condicionUno    &&  condicionDos            ||               !
-    y logica                                o logica         negacion
-    */
