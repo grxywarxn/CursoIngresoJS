cff9f84dcaa722f0f28adb554262b4560c99fb75
grxywarxn <lclaros800@gmail.com>
2023-02-27 10:48:36 -0300

    sabado ej 3 preparcial

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,21 +1,40 @@
 //Luis Claros
+/* * Apellido:
+ * Nombre:
+ * Comisión:
+ * Tutor:
+ 
+Ejercicio 3
 
+Una fabrica de muebles nos pide realizar una app para gestionar sus productos. 
+El programa pide dar de alta cada venta realizada (Cada venta representa un mueble 
+en particular, con los atributos definidos mas abajo).
+
+Se debe ingresar en cada venta.
+-Nombre del mueble
+-Material del mueble (Madera, Metal y Vidrio)
+-Peso
+-Altura
+-Anchura 
+-Precio
+
+Debido a la tala excesiva de árboles el gobierno pone un impuesto del 15% ante la compra de 
+muebles de madera.
+Los muebles de vidrio o metal tienen un 10% de descuento.
+
+Se pide
+
+a)Precio promedio por mueble vendido
+b)Cantidad total de muebles de madera
+c)El mueble de metal más alto 
+d)El mueble más liviano
+e)Porcentaje de muebles de cada tipo de material
+f)Monto total recaudado bruto de todos los muebles (sin descuentos ni impuestos)
+g)Monto total recaudado, pero solo de los muebles de madera(incluido descuentos e impuestos)
+
+NOTA: Los precios no podran ser cero ni negativo.
+ */
 function mostrar()
 {
-	let edad;
-	let mensaje;
-
-	edad = parseInt(document.getElementById("txtIdEdad").value);
-
-	if(edad >= 18){
-
-		mensaje = "Usted es mayor de edad";
-
-	}else{
-
-		mensaje = "Usted es menor de edad";
-
-	}
-
-	alert(mensaje);
+	
 }//FIN DE LA FUNCIÓN
