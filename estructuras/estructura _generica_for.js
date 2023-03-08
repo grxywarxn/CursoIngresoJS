fbe01fae6d9a55ad49d2e79c62d372d603cbdcf7
grxywarxn <lclaros800@gmail.com>
2023-03-08 08:03:22 -0300

    FOR

--- estructuras/estructura _generica_for.js
+++ estructuras/estructura _generica_for.js
@@ -3,10 +3,10 @@ for (let i = 0; i < 10; i++) {
     while (!isNaN(ingNombre)) {
         ingNombre = prompt("Error. Ingrese su nombre: ");
     }
-    x = prompt("ingrese el tipo de producto: 'x1', 'x2' o 'x3'");
+    x = prompt("Ingrese el tipo de producto: 'x1', 'x2' o 'x3'");
     x = x.toLowerCase();
     while (x != "x1" && x != "x2" && x != "x3") {
-        x = prompt("Ingrese el tipo de producto: 'x1', 'x2' o 'x3'")
+        x = prompt("Error. Ingrese el tipo de producto: 'x1', 'x2' o 'x3'")
     }
     ingPrecioUnitario = parseFloat(prompt("Ingrese el precio unitario: (entre 1  y 1000)"));
     while (isNaN(ingPrecioUnitario) || ingPrecioUnitario < 1 || ingPrecioUnitario > 10) {
